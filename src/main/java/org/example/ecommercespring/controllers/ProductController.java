eb53a02c0b743b9131498795498a1001b82fe3f9
Pritesh Kumar <pritesh.kumar2@gmail.com>
2025-07-08 09:56:37 +0530

    add repo impl

--- src/main/java/org/example/ecommercespring/controllers/ProductController.java
+++ src/main/java/org/example/ecommercespring/controllers/ProductController.java
@@ -1,20 +1,26 @@
 package org.example.ecommercespring.controllers;
 
 import org.example.ecommercespring.dto.ProductDTO;
+import org.example.ecommercespring.entity.Product;
+import org.example.ecommercespring.repository.ProductRepository;
 import org.example.ecommercespring.services.ICategoryService;
 import org.example.ecommercespring.services.IProductService;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
     private final IProductService productService;
+    private final ProductRepository productRepository;
 
-    public ProductController(IProductService productService) {
+    public ProductController(IProductService productService,
+                             ProductRepository productRepository) {
         this.productService = productService;
+        this.productRepository = productRepository;
     }
 
     @GetMapping("/{id}")
@@ -23,8 +29,40 @@ public class ProductController {
         return this.productService.getProduct(id);
     }
 
+    @GetMapping("/product/{id}")
+    public Product getSingleProductById(@PathVariable long id) throws IOException {
+        //return "ID: " + id;
+        //return this.productService.getProduct(id);
+        Optional<Product> product = this.productRepository.findById(id);
+        System.out.println(product.get());
+        return product.get();
+    }
+
+    @GetMapping("/product/category")
+    public List<Product> getAllProductsByCategory(@RequestParam String type) throws IOException {
+        return this.productRepository.findByCategory(type);
+    }
+
     @GetMapping("/category")
     public List<ProductDTO> getProductsByCategory(@RequestParam String type) throws IOException {
         return this.productService.getAllProductsByCategory(type);
     }
+
+    @PostMapping("/product")
+    public Product newProduct(@RequestBody ProductDTO productDTO) {
+        Product product =
+                Product.builder().title(productDTO.getTitle())
+                        .brand(productDTO.getBrand())
+                        .color(productDTO.getColor())
+                        .model(productDTO.getModel())
+                        .price(Integer.parseInt(productDTO.getPrice()))
+                        .category(productDTO.getCategory())
+                        .description(productDTO.getDescription())
+                        .discount(Integer.parseInt(productDTO.getDiscount()))
+                        .popular(productDTO.isPopular())
+                        .image(productDTO.getImage()).build();
+
+        return productRepository.save(product);
+    }
+
 }
---
+++ src/main/java/org/example/ecommercespring/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package org.example.ecommercespring.repository;
+
+import org.example.ecommercespring.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+    List<Product> findByCategory(String category);
+}
